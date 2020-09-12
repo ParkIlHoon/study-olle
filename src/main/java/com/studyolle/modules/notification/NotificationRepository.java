418fec592b21277c688c7ec7b3d89910943b2c69
ParkIlHoon <chiwoo2074@gmail.com>
2020-09-12 15:47:41 +0900

    알림 아이콘 변경 기능 구현

--- src/main/java/com/studyolle/modules/notification/NotificationRepository.java
+++ src/main/java/com/studyolle/modules/notification/NotificationRepository.java
@@ -1,7 +1,9 @@
 package com.studyolle.modules.notification;
 
+import com.studyolle.modules.account.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface NotificationRepository extends JpaRepository<Notification, Long>
 {
+    int countByAccountAndChecked(Account account, boolean b);
 }
