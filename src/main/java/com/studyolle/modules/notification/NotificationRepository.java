a6df1e78a758a6049834d076187f2b13e30de415
ParkIlHoon <chiwoo2074@gmail.com>
2020-09-12 17:58:30 +0900

    읽은 알림 제거

--- src/main/java/com/studyolle/modules/notification/NotificationRepository.java
+++ src/main/java/com/studyolle/modules/notification/NotificationRepository.java
@@ -2,9 +2,11 @@ package com.studyolle.modules.notification;
 
 import com.studyolle.modules.account.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
+@Transactional
 public interface NotificationRepository extends JpaRepository<Notification, Long>
 {
     int countByAccountAndChecked(Account account, boolean b);
